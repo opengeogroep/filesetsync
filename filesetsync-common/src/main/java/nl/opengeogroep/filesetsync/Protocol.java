994fcabfe695990b6075a717b4a00b1dfa0150b9
Matthijs Laan <matthijsln@gmail.com>
2014-06-30 18:05:04 +0200

    fixed fileset list decoder and added encoder

--- filesetsync-common/src/main/java/nl/opengeogroep/filesetsync/Protocol.java
+++ filesetsync-common/src/main/java/nl/opengeogroep/filesetsync/Protocol.java
@@ -2,11 +2,14 @@
 package nl.opengeogroep.filesetsync;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -14,25 +17,64 @@ import java.util.List;
  * @author Matthijs Laan
  */
 public class Protocol {
+    public static final String FILELIST_ENCODING = "UTF-8";
+
     public static List<FileRecord> decodeFilelist(InputStream in) throws IOException {
         // Poorly-encoded text decoding
-        
+
         List<FileRecord> l = new ArrayList();
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(in, FILELIST_ENCODING));
         String line = br.readLine();
         if(line == null || !"filesetsync:filelist".equals(line)) {
             throw new IOException("Wrong filelist format returned, bad server URL?");
         }
         while((line = br.readLine()) != null) {
-            String[] s = line.split("|");
+            String[] s = line.split("\\|");
             FileRecord r = new FileRecord();
             r.setType(s[0].charAt(0));
             r.setName(s[1]);
             r.setSize(Long.parseLong(s[2]));
-            r.setLastModified(new Date(Long.parseLong(s[3])));
-            r.setChecksum(s[4]);
+            r.setLastModified(Long.parseLong(s[3]));
+            r.setHash("null".equals(s[4]) ? null : s[4]);
             l.add(r);
         }
         return l;
-    }    
+    }
+
+    public static class BufferedFileRecordEncoder {
+        private BufferedWriter writer = null;
+
+        private boolean headerWritten = false;
+
+        public BufferedFileRecordEncoder(OutputStream out) {
+            try {
+                this.writer = new BufferedWriter(new OutputStreamWriter(out, FILELIST_ENCODING));
+            } catch (UnsupportedEncodingException ex) {
+            }
+        }
+
+        public void write(FileRecord f) throws IOException {
+            if(!headerWritten) {
+                writer.append("filesetsync:filelist\n");
+                headerWritten = true;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.append(f.getType());
+            sb.append("|");
+            sb.append(f.getName());
+            sb.append("|");
+            sb.append(f.getSize());
+            sb.append("|");
+            sb.append(f.getLastModified());
+            sb.append("|");
+            sb.append(f.getHash() != null ? f.getHash() : "null");
+            sb.append("\n");
+            writer.append(sb);
+        }
+
+        public void flush() throws IOException {
+            writer.flush();
+        }
+    }
 }
