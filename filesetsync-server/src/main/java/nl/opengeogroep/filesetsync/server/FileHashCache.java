065ea4639eb6f5c2ccd47e85999f0c460b748c50
Matthijs Laan <matthijsln@gmail.com>
2014-07-02 10:07:56 +0200

    persist ehcache on contextDestroyed and load persisted cache in contextInitialized, use time to idle instead of time to live

--- filesetsync-server/src/main/java/nl/opengeogroep/filesetsync/server/FileHashCache.java
+++ filesetsync-server/src/main/java/nl/opengeogroep/filesetsync/server/FileHashCache.java
@@ -1,9 +1,17 @@
 package nl.opengeogroep.filesetsync.server;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import net.sf.ehcache.Cache;
@@ -16,7 +24,9 @@ import net.sf.ehcache.config.PersistenceConfiguration;
 import net.sf.ehcache.config.PersistenceConfiguration.Strategy;
 import net.sf.ehcache.store.MemoryStoreEvictionPolicy;
 import nl.opengeogroep.filesetsync.FileRecord;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.commons.lang3.mutable.MutableLong;
+import org.apache.commons.lang3.time.DurationFormatUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -49,8 +59,17 @@ public class FileHashCache implements ServletContextListener {
     public static Map<String,CacheManager> cacheManagers = new HashMap();
     public static Map<String,Cache> caches = new HashMap();
 
+    private static String getCacheDir(String name) {
+        return ServerSyncConfig.getInstance().getFileset(name).getPath() + File.separator + CACHE_DIR;
+    }
+
+    private static String getPersistedCacheFile(String name) {
+        return getCacheDir(name) + File.separator + name + "-hashes.txt.gz";
+    }
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        log.info("*** initializing ***");
 
         int maxElementsInMemory = 10000;
         try {
@@ -69,7 +88,7 @@ public class FileHashCache implements ServletContextListener {
             if(f.isFile()) {
                 continue;
             }
-            File cacheDir = new File(fs.getPath() + File.separator + CACHE_DIR);
+            File cacheDir = new File(getCacheDir(name));
             log.info("Creating Ehcache for fileset " + fs.getName() + " in directory " + cacheDir);
             Configuration cacheManagerConfig = new Configuration()
                     .name(name)
@@ -78,40 +97,91 @@ public class FileHashCache implements ServletContextListener {
             CacheConfiguration cacheConfig = new CacheConfiguration(name, maxElementsInMemory)
                     .memoryStoreEvictionPolicy(MemoryStoreEvictionPolicy.LFU)
                     .eternal(false)
-                    .timeToLiveSeconds(60 * 60 * 24)
+                    .timeToIdleSeconds(60 * 60 * 24)
                     .persistence(new PersistenceConfiguration().strategy(Strategy.LOCALTEMPSWAP));
             cacheManagerConfig.addCache(cacheConfig);
 
             CacheManager cacheManager = new CacheManager(cacheManagerConfig);
             cacheManagers.put(name, cacheManager);
-            caches.put(name, cacheManager.getCache(name));
-            // TODO read persisted cache
+            Cache cache = cacheManager.getCache(name);
+            caches.put(name, cache);
+
+            File persistedCache = new File(getPersistedCacheFile(name));
+            if(persistedCache.exists() && persistedCache.canRead()) {
+                log.info(String.format("Reading persisted cache for \"%s\" from \"%s\"...", name, persistedCache));
+                try (
+                    BufferedReader br = new BufferedReader(
+                            new InputStreamReader(
+                                    new GZIPInputStream(
+                                        new FileInputStream(persistedCache))))) {
+
+                    long startTime = System.currentTimeMillis();
+
+                    String line;
+                    while((line = br.readLine()) != null) {
+                        String[] s = line.split(":");
+                        cache.put(new Element(s[0], s[1]));
+                    }
+                    log.info(String.format("Cache size is %d, read in %s", cache.getSize(), DurationFormatUtils.formatDurationWords(System.currentTimeMillis() - startTime, true, false)));
+                } catch(IOException e) {
+                    log.error("Exception reading persisted cache", e);
+                }
+            }
         }
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        for(CacheManager cm: cacheManagers.values()) {
-            // TODO: persist cache
-            cm.shutdown();
+        log.info("*** destroying ***");
+
+        for(Map.Entry<String,CacheManager> entry: cacheManagers.entrySet()) {
+            String name = entry.getKey();
+            CacheManager manager = entry.getValue();
+            Cache cache = manager.getCache(name);
+            log.info(String.format("Evicting expired elements from cache \"%s\"", name));
+            cache.evictExpiredElements();
+            log.info(String.format("Persisting cache \"%s\" (size %d)...", name, cache.getSize()));
+            long startTime = System.currentTimeMillis();
+            try (BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(
+                            new GZIPOutputStream(
+                                    new FileOutputStream(getPersistedCacheFile(name)))))) {
+                for(Object key: cache.getKeys()) {
+                    Element e = cache.get(key);
+                    if(e != null) {
+                        writer.write(key + ":" + e.getObjectValue());
+                        writer.newLine();
+                    }
+                }
+                log.info("Done persisting cache in " + DurationFormatUtils.formatDurationWords(System.currentTimeMillis() - startTime, true, false));
+            } catch(IOException e) {
+                log.error(String.format("Error persisting cache \"%s\": %s", name, ExceptionUtils.getMessage(e)), e);
+            }
+            manager.shutdown();
         }
     }
 
     public static String getCachedFileHash(ServerFileset fileset, File f, long fileLastModified, MutableLong hashBytesAccumulator, MutableLong hashTimeMillisAccumulator) throws IOException {
         Cache cache = caches.get(fileset.getName());
-        Element e  = cache.get(f.getCanonicalPath());
+        String canonicalPath = f.getCanonicalPath();
+        Element e  = cache.get(canonicalPath);
         String hash = null;
         if(e != null) {
             String[] parts = ((String)e.getObjectValue()).split(",");
-            long lastModified = Long.parseLong(parts[0]);
-            if(lastModified == fileLastModified) {
-                hash = parts[1];
+            try {
+                long lastModified = Long.parseLong(parts[0]);
+                if(lastModified == fileLastModified) {
+                    hash = parts[1];
+                }
+            } catch(java.lang.ArrayIndexOutOfBoundsException ex) {
+                log.error("Index OOB for line \"" + e.getObjectValue());
+                throw ex;
             }
         }
         if(hash == null) {
             hash = FileRecord.calculateHash(f, hashTimeMillisAccumulator);
             hashBytesAccumulator.add(f.length());
-            cache.put(new Element(f.getCanonicalPath(), f.lastModified() + "," + hash));
+            cache.put(new Element(canonicalPath, fileLastModified + "," + hash));
         }
         return hash;
     }
