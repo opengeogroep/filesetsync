1b3597eccc289b15500456556bc16f35bc60783c
Matthijs Laan <matthijsln@gmail.com>
2016-09-12 12:24:30 +0200

    fix error with reporting once scheduled jobs

--- filesetsync-client/src/main/java/nl/opengeogroep/filesetsync/client/SyncJobState.java
+++ filesetsync-client/src/main/java/nl/opengeogroep/filesetsync/client/SyncJobState.java
@@ -338,7 +338,7 @@ public class SyncJobState implements Serializable {
     }
 
     public Date calculateNextRunDate(Fileset fs) {
-        if(lastRun == null || UNFINISHED_STATES.contains(currentState)) {
+        if(lastRun == null || UNFINISHED_STATES.contains(currentState) || SCHEDULE_HOURLY.equals(fs.getSchedule())) {
             return null;
         } else {
             Calendar c = GregorianCalendar.getInstance();
