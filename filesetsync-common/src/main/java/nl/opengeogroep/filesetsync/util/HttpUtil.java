3fcdb66169fdc6af48d3405492d3d3bf7a97c4c8
Matthijs Laan <matthijsln@gmail.com>
2018-10-19 11:53:40 +0200

    fix unsafe multithreaded synchronisation

--- filesetsync-common/src/main/java/nl/opengeogroep/filesetsync/util/HttpUtil.java
+++ filesetsync-common/src/main/java/nl/opengeogroep/filesetsync/util/HttpUtil.java
@@ -14,17 +14,21 @@ import org.apache.http.protocol.HttpDateGenerator;
  * @author Matthijs Laan
  */
 public class HttpUtil {
-    private static final DateFormat format = new SimpleDateFormat(HttpDateGenerator.PATTERN_RFC1123, Locale.US);
+    private DateFormat format = null;
 
-    static {
-        format.setTimeZone(TimeZone.getTimeZone("GMT"));
+    private DateFormat getFormat() {
+        if(format == null) {
+            format = new SimpleDateFormat(HttpDateGenerator.PATTERN_RFC1123, Locale.US);
+            format.setTimeZone(TimeZone.getTimeZone("GMT"));
+        }
+        return format;
     }
 
-    public static String formatDate(Date date) {
-        return format.format(date);
+    public String formatDate(Date date) {
+        return getFormat().format(date);
     }
 
-    public static Date parseDate(String dateString) throws ParseException {
-        return format.parse(dateString);
+    public Date parseDate(String dateString) throws ParseException {
+        return getFormat().parse(dateString);
     }
 }
